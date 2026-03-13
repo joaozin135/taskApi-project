6ead45aa82ae26920077b957eaa1e3c12f064b0e
joaozin135 <junior.joao1305@gmail.com>
2026-03-13 01:24:20 -0300

    Update user.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID, createHash } from 'crypto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './models/user.model';
+import { OmitType } from '@nestjs/mapped-types';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [];
+    
+    create(dto: CreateUserDto){
+        const newUser: User ={
+            id: randomUUID(),
+            name: dto.name,
+            email: dto.email.toLocaleLowerCase(),
+            passwordHash: createHash('sha256').update(dto.password).digest('hex')
+        };
+        this.users.push(newUser);
+        const{passwordHash, ...userSemPassword} = newUser;
+        return userSemPassword;
+    }
+    findAll(){
+        return this.users.map(({passwordHash, ...safeUser})=> safeUser);
+    }
+}
