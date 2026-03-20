0755cd92093a15a0ef7853b0a62e4799ee6477f9
joaozin135 <junior.joao1305@gmail.com>
2026-03-19 22:13:32 -0300

    add UserExistsGuard

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UserExistsGuard } from 'src/common/guards/user-exists';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
     @Post()
+    @UserGuards(UserExistsGuard)
     create(@Body() CreateTaskDto: CreateTaskDto) {
         return this.tasksService.create(CreateTaskDto);
     }
