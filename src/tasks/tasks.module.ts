a0e28973d4c6164138fdb075aac5cb10df7e1623
joaozin135 <junior.joao1305@gmail.com>
2026-03-19 22:12:05 -0300

    add providers UserExistsGuard

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { UsersModule } from 'src/users/users.module';
+import { UserExistsGuard } from 'src/common/guards/user-exists';
 
 @Module({
   imports: [UsersModule],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService, UserExistsGuard]
 })
 
 export class TasksModule {}
